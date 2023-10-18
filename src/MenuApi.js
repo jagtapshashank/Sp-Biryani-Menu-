3a1aae8d7307a7b54f862659e0c469676680eca2
Shashank Jagtap <shashankjagtap@Shashanks-MacBook-Air.local>
2023-10-17 22:07:04 -0500

    Made changes to run on MAC

---
+++ src/MenuApi.js
@@ -0,0 +1,468 @@
+const Menu = [
+    {
+        id: 1,
+        image: "images/1.jpg",
+        name: "Sajuk Ghee Mutton Dum Biryani",
+        category: "biryani",
+        price: "410₹",
+        description:
+            "",
+    },
+
+    {
+        id: 2,
+        image: "images/2.jpg",
+        name: "Amir Khan Special Mutton Dum Biryani",
+        category: "biryani",
+        price: "430₹",
+        description:
+          "",
+    },
+
+    {
+        id: 3,
+        image: "images/3.jpg",
+        name: "SP's Mutton Dum Biryani",
+        category: "biryani",
+        price: "380₹",
+        description:
+          "",
+    },
+
+    {
+        id: 4,
+        image: "images/4.jpg",
+        name: "Mutton Kacchi Dum Biryani",
+        category: "biryani",
+        price: "380₹",
+        description:
+          "",
+    },
+
+    {
+        id: 5,
+        image: "images/5.jpg",
+        name: "Gavran Chicken Dum Biryani",
+        category: "biryani",
+        price: "360₹",
+        description:
+          "",
+    },
+
+    {
+        id: 6,
+        image: "images/6.jpg",
+        name: "Broiler Chicken Dum Biryani",
+        category: "biryani",
+        price: "380₹",
+        description:
+          "",
+    },
+
+    {
+        id: 7,
+        image: "images/7.jpg",
+        name: "Broiler Chicken Sajuk Ghee Dum Biryani",
+        category: "biryani",
+        price: "400₹",
+        description:
+          "",
+    },
+
+    {
+        id: 8,
+        image: "images/8.jpg",
+        name: "Mutton Pulav",
+        category: "biryani",
+        price: "380₹",
+        description:
+          "",
+    },
+
+    {
+        id: 9,
+        image: "images/9.jpg",
+        name: "Prawns Pulav",
+        category: "biryani",
+        price: "480₹",
+        description:
+          "",
+    },
+
+    {
+        id: 10,
+        image: "images/10.jpg",
+        name: "Eggs Biryani",
+        category: "biryani",
+        price: "280₹",
+        description:
+          "",
+    },
+
+    {
+        id: 11,
+        image: "images/11.jpg",
+        name: "Full Murg Dum Biryani(One day on advance order)",
+        category: "biryani",
+        price: "1100₹",
+        description:
+          "",
+    },
+
+    {
+        id: 12,
+        image: "images/12.jpg",
+        name: "Full Leg Mutton Dum Biryani(One day on advance order)",
+        category: "biryani",
+        price: "2100₹",
+        description:
+          "",
+    },
+
+    {
+        id: 13,
+        image: "images/13.jpg",
+        name: "Mutton Alani Soup",
+        category: "Mutton",
+        price: "170₹",
+        description:
+          "",
+    },
+
+    {
+        id: 14,
+        image: "images/14.jpg",
+        name: "Mutton Paya Soup",
+        category: "Mutton",
+        price: "170₹",
+        description:
+          "",
+    },
+
+    {
+        id: 15,
+        image: "images/15.jpg",
+        name: "Mutton Chop Masala/Fry",
+        category: "Mutton",
+        price: "460₹",
+        description:
+          "",
+    },
+
+    {
+        id: 16,
+        image: "images/16.jpg",
+        name: "Mutton Handi(Full)",
+        category: "Mutton",
+        price: "900₹",
+        description:
+          "",
+    },
+
+    {
+        id: 17,
+        image: "images/17.jpg",
+        name: "Mutton Handi(Half)",
+        category: "Mutton",
+        price: "450₹",
+        description:
+          "",
+    },
+
+    {
+        id: 18,
+        image: "images/18.jpg",
+        name: "Mutton Masala",
+        category: "Mutton",
+        price: "370₹",
+        description:
+          "",
+    },
+
+    {
+        id: 19,
+        image: "images/19.jpg",
+        name: "Liver Fry",
+        category: "Mutton",
+        price: "360₹",
+        description:
+          "",
+    },
+
+    {
+        id: 20,
+        image: "images/20.jpg",
+        name: "Bheja Fry",
+        category: "Mutton",
+        price: "270₹",
+        description:
+          "",
+    },
+
+    {
+        id: 21,
+        image: "images/21.jpg",
+        name: "Eggs Masala",
+        category: "Eggs",
+        price: "280₹",
+        description:
+          "",
+    },
+
+    {
+        id: 22,
+        image: "images/22.jpg",
+        name: "Eggs Omlet",
+        category: "Eggs",
+        price: "100₹",
+        description:
+          "",
+    },
+
+    {
+        id: 23,
+        image: "images/23.jpg",
+        name: "Eggs Burji",
+        category: "Eggs",
+        price: "100₹",
+        description:
+          "",
+    },
+
+    {
+        id: 24,
+        image: "images/24.jpg",
+        name: "Eggs Half Fry",
+        category: "Eggs",
+        price: "100₹",
+        description:
+          "",
+    },
+    
+    {
+        id: 25,
+        image: "images/25.jpg",
+        name: "Eggs Boil",
+        category: "Eggs",
+        price: "70₹",
+        description:
+          "",
+    },
+
+    {
+        id: 26,
+        image: "images/26.jpg",
+        name: "Bajari Bhakri",
+        category: "Tandoor",
+        price: "45₹",
+        description:
+          "",
+    },
+
+    {
+        id: 27,
+        image: "images/27.jpg",
+        name: "Jawari Bhakri",
+        category: "Tandoor",
+        price: "45₹",
+        description:
+          "",
+    },
+
+    {
+        id: 28,
+        image: "images/28.jpg",
+        name: "Chappati",
+        category: "Tandoor",
+        price: "30₹",
+        description:
+          "",
+    },
+
+    {
+        id: 29,
+        image: "images/29.jpg",
+        name: "Laccha Paratha",
+        category: "Tandoor",
+        price: "40₹",
+        description:
+          "",
+    },
+
+    {
+        id: 30,
+        image: "images/30.jpg",
+        name: "Butter Laccha Paratha",
+        category: "Tandoor",
+        price: "50₹",
+        description:
+          "",
+    },
+
+    {
+        id: 31,
+        image: "images/31.jpg",
+        name: "Cold-Drinks(300ml)",
+        category: "Beverages",
+        price: "30₹",
+        description:
+          "",
+    },
+
+    {
+        id: 32,
+        image: "images/32.jpg",
+        name: "Mineral Water",
+        category: "Beverages",
+        price: "35₹",
+        description:
+          "",
+    },
+
+    {
+        id: 33,
+        image: "images/33.jpg",
+        name: "Solkadhi",
+        category: "Beverages",
+        price: "50₹",
+        description:
+          "",
+    },
+
+    {
+        id: 34,
+        image: "images/34.jpg",
+        name: "Sp. Gavaran Chicken Handi (Full)",
+        category: "Chicken",
+        price: "800₹",
+        description:
+          "",
+    },
+
+    {
+        id: 35,
+        image: "images/35.jpg",
+        name: "Sp. Gavaran Chicken Handi (Half)",
+        category: "Chicken",
+        price: "410₹",
+        description:
+          "",
+    },
+
+    {
+        id: 36,
+        image: "images/36.jpg",
+        name: "Gavaran Chicken Masala",
+        category: "Chicken",
+        price: "380₹",
+        description:
+          "",
+    },
+
+    {
+        id: 37,
+        image: "images/37.jpg",
+        name: "Gavaran Chicken Fry",
+        category: "Chicken",
+        price: "380₹",
+        description:
+          "",
+    },
+
+    {
+        id: 38,
+        image: "images/38.jpg",
+        name: "Chicken Lollipop",
+        category: "Chicken",
+        price: "290₹",
+        description:
+          "",
+    },
+
+    {
+        id: 39,
+        image: "images/39.jpg",
+        name: "Pomfret Masala",
+        category: "Fish",
+        price: "520₹",
+        description:
+          "",
+    },
+
+    {
+        id: 40,
+        image: "images/40.jpg",
+        name: "Surmai Curry",
+        category: "Fish",
+        price: "480₹",
+        description:
+          "",
+    },
+
+    {
+        id: 41,
+        image: "images/41.jpg",
+        name: "Prawns Masala",
+        category: "Fish",
+        price: "500₹",
+        description:
+          "",
+    },
+
+    {
+        id: 42,
+        image: "images/42.jpg",
+        name: "Promfret Fry",
+        category: "Fish",
+        price: "520₹",
+        description:
+          "",
+    },
+
+    {
+        id: 43,
+        image: "images/43.jpg",
+        name: "Surmai Fry",
+        category: "Fish",
+        price: "500₹",
+        description:
+          "",
+    },
+
+    {
+        id: 44,
+        image: "images/44.jpg",
+        name: "Prawns Fry",
+        category: "Fish",
+        price: "500₹",
+        description:
+          "",
+    },
+
+    {
+        id: 45,
+        image: "images/45.jpg",
+        name: "Bombil Chatani",
+        category: "Chatani",
+        price: "270₹",
+        description:
+          "",
+    },
+
+    {
+        id: 46,
+        image: "images/46.jpg",
+        name: "Sukat Chatani",
+        category: "Chatani",
+        price: "240₹",
+        description:
+          "",
+    },
+
+    
+
+  
+    
+  ];
+  
+  export default Menu
